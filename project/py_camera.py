f72eadd07600cdd4a5fcc2c3fb1822fe7f1cc383
steve <stxa1234@colorado.edu>
2019-12-11 08:53:24 -0700

    before adding comments

--- project/py_camera.py
+++ project/py_camera.py
@@ -6,11 +6,20 @@ import boto3
 def get_label():
 	image = cam.PiCamera()
 	image.vflip = True
-	image.capture('test.jpg')
+	image.capture('IMG_3125.JPG')
 
 	img = "test.jpg"
 
 	client = boto3.client("rekognition", region_name='us-east-1')
+
+	#upload the image to bucket
+	# Create an S3 client
+	S3_upld = boto3.client('s3')
+
+	BUCKET_NAME = "datasteve"
+	SOURCE_FILENAME = "IMG_3125.JPG"
+
+	S3_upld.upload_file(SOURCE_FILENAME, BUCKET_NAME, SOURCE_FILENAME)
 
 	with open(img, "rb") as source_image:
 	    source_bytes = source_image.read()
