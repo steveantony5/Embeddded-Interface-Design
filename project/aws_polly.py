fb5fb1de3f2e88e67ac6fdc93b79cf5942ac6258
Steve Antony <43216285+steveantony5@users.noreply.github.com>
2019-12-11 00:57:25 -0700

    Update aws_polly.py

--- project/aws_polly.py
+++ project/aws_polly.py
@@ -2,14 +2,8 @@ import boto3
 
 polly_client = boto3.Session(region_name='us-east-1').client('polly')
 
-response_polly = polly_client.synthesize_speech(VoiceId='Joanna',
-                OutputFormat='mp3', 
-                Text = 'Wand says ')
+response = polly_client.synthesize_speech(VoiceId='Joanna',OutputFormat='mp3', Text = 'steve steve ')
                 
-# Output the task ID
-taskId = response['SynthesisTask']['TaskId']
-print(f'Task id is {taskId}')
-
-# Retrieve and output the current status of the task
-task_status = polly_client.get_speech_synthesis_task(TaskId = taskId)
-print(f'Status: {task_status}')
+file = open('speech.mp3', 'wb')
+file.write(response['AudioStream'].read())
+file.close()
